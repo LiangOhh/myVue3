70bb370b13278dcd69a55f442eb3547243e6c7b4
Liang <Liang@163.com>
2024-04-17 18:41:58 +0800

    推送的第一个vue3项目

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="my">
+    这是我的第一个vue3项目
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "App"
+}
+
+</script>
+
+<style scoped>
+.my {
+  margin-top: 10px;
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 20px;
+  padding: 20px;
+}
+</style>
