acc34c32fcd0e3648bd24fcc7f60200ff81c4605
Liang <Liang@163.com>
2024-05-24 22:10:55 +0800

    3

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,11 @@ import vue from '@vitejs/plugin-vue'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     vue(),
     VueSetupExtend()
   ],
-  base: '/'
-  ,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
